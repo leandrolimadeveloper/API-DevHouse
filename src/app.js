7f7fdb04ba6e014ff353d74c9959281f1404e223
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-04-08 14:08:39 -0300

    MongoDB - Configurações

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import routes from './routes'
 
 class App {
     constructor() {
         this.server = express()
-       
+        
+        mongoose.connect('mongodb+srv://devhouse:devhouse_pass@cursojs.cf0wr.mongodb.net/devhouse?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
         this.middlewares()
         this.routes()
     }
+
 
     middlewares() {
         this.server.use(express.json())
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const routes = new Router()
 
 routes.get('/', (req, res) => {
-    return res.json({OK: true})
+    return res.json({OK: false})
 })
 
 export default routes
