4e223d3fa19af85d15bd831149d7fa3a37df1565
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-04-14 21:10:10 -0300

    Validando informações

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,8 +1,17 @@
 import User from '../models/User'
+import * as Yup from 'yup'
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required()
+        })
+        
         const email = req.body.email
+
+        if(!(await schema.isValid(req.body))) {
+            return res.status(400).json({error: 'Falha na validação'})
+        }
 
         // Verificar se email já existe no BD
         let user = await User.findOne({ email })
